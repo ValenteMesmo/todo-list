1260597f5f7d7daac6aecbd2429ccc7b0f706b18
valentemesmo <valentemesmo@gmail.com>
2020-11-08 19:14:06 -0300

    initial commit :feelsgood:

---
+++ src/app/services/sound-service.ts
@@ -0,0 +1,30 @@
+export class SoundService {
+
+    private static audio;
+    private static audio2;
+
+    static play() {
+        if (!SoundService.audio) {
+            SoundService.audio = new Audio();
+            SoundService.audio.src = '../assets/completetask_0.mp3';
+
+            SoundService.audio.load();
+        }
+        SoundService.audio.pause();
+        SoundService.audio.currentTime = 0;
+        SoundService.audio.play();
+    }
+
+    static play2() {
+        if (!SoundService.audio2) {
+            SoundService.audio2 = new Audio();
+            SoundService.audio2.src = '../assets/gmae.wav';
+
+            SoundService.audio2.load();
+        }
+        SoundService.audio2.pause();
+        SoundService.audio2.currentTime = 0;
+        SoundService.audio2.play();
+    }
+
+}
