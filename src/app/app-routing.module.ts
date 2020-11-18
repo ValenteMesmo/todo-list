a99e4bb36da7b00dbcfd4b5241889dcf8257ba51
valentemesmo <valentemesmo@gmail.com>
2020-11-18 10:33:13 -0300

    including another route redirect

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DoingPageComponent } from './pages/doing-page/doing-page';
 import { TodoPageComponent } from './pages/todo-page/todo-page';
-import { PLANNING_ROUTE, DOING_ROUTE_NAME, PLANNING_ROUTE_NAME } from './shared/route-constants';
+import { PLANNING_ROUTE, DOING_ROUTE_NAME, PLANNING_ROUTE_NAME, REPOSITORY_NAME } from './shared/route-constants';
 
 const routes: Routes = [
   { path: '', redirectTo: PLANNING_ROUTE, pathMatch: 'full' }
+  , { path: REPOSITORY_NAME, redirectTo: PLANNING_ROUTE, pathMatch: 'full' }
   , { path: DOING_ROUTE_NAME, component: DoingPageComponent }
   , { path: PLANNING_ROUTE_NAME, component: TodoPageComponent }
 ];
--- src/app/shared/route-constants.ts
+++ src/app/shared/route-constants.ts
@@ -1,4 +1,5 @@
-export const PLANNING_ROUTE_NAME = 'todo-list/planning';
-export const DOING_ROUTE_NAME = 'todo-list/doing';
+export const REPOSITORY_NAME = 'todo-list';
+export const PLANNING_ROUTE_NAME = `${REPOSITORY_NAME}/planning`;
+export const DOING_ROUTE_NAME = `${REPOSITORY_NAME}/doing`;
 export const PLANNING_ROUTE = `/${PLANNING_ROUTE_NAME}`;
 export const DOING_ROUTE = `/${DOING_ROUTE_NAME}`;
