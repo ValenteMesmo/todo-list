d5d3c895cb691fb649be574c71cbc2285ca8bc1e
valentemesmo <valentemesmo@gmail.com>
2020-11-19 10:05:22 -0300

    fixing calls to isDevMode

--- src/app/services/sound-service.ts
+++ src/app/services/sound-service.ts
@@ -5,15 +5,15 @@ export class SoundService {
   
   private static audio;
   private static audio2;
-  private static readonly BASE_FILE_PATH = isDevMode() ? '' : `${REPOSITORY_NAME}/`;
+  private static BASE_FILE_PATH = ()=> isDevMode() ? '' : `${REPOSITORY_NAME}/`;
 
   static preLoad() {
     SoundService.audio = new Audio();
-    SoundService.audio.src = `./${this.BASE_FILE_PATH}assets/completetask_0.mp3`;
+    SoundService.audio.src = `./${this.BASE_FILE_PATH()}assets/completetask_0.mp3`;
     SoundService.audio.load();
 
     SoundService.audio2 = new Audio();
-    SoundService.audio2.src = `./${this.BASE_FILE_PATH}assets/gmae.wav`;
+    SoundService.audio2.src = `./${this.BASE_FILE_PATH()}assets/gmae.wav`;
     SoundService.audio2.load();
   }
 
@@ -21,7 +21,7 @@ export class SoundService {
   static play() {
     if (!SoundService.audio) {
       SoundService.audio = new Audio();
-      SoundService.audio.src = `./${this.BASE_FILE_PATH}assets/completetask_0.mp3`;
+      SoundService.audio.src = `./${this.BASE_FILE_PATH()}assets/completetask_0.mp3`;
 
       SoundService.audio.load();
     }
@@ -33,7 +33,7 @@ export class SoundService {
   static play2() {
     if (!SoundService.audio2) {
       SoundService.audio2 = new Audio();
-      SoundService.audio2.src = `./${this.BASE_FILE_PATH}assets/gmae.wav`;
+      SoundService.audio2.src = `./${this.BASE_FILE_PATH()}assets/gmae.wav`;
 
       SoundService.audio2.load();
     }
