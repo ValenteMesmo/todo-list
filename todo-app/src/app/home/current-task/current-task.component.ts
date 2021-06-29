bff94d218a486181f7f90be61d6e9127c3374d51
Ricardo Valente <valentemesmo@gmail.com>
2021-06-28 22:39:29 -0300

    changing current task acording to pomodoro state

--- todo-app/src/app/home/current-task/current-task.component.ts
+++ todo-app/src/app/home/current-task/current-task.component.ts
@@ -1,5 +1,5 @@
 import { Component } from "@angular/core";
-import { EventType } from "../../_shared/services/event-processor";
+import { EventType, Task, TaskType } from "../../_shared/services/event-processor";
 import { EventService } from "../../_shared/services/event-service";
 
 @Component({
@@ -8,14 +8,38 @@ import { EventService } from "../../_shared/services/event-service";
   styleUrls: ['current-task.component.scss'],
 })
 export class CurrentTaskComponent {
-  constructor(
-    protected readonly service: EventService) { }
+  pomodoroTask: Task;
+  breakTask: Task;
+  longBreakTask: Task;
+
+  constructor(public service: EventService) {
+
+    service.processor.onEventsChanged.subscribe(f => {
+      this.pomodoroTask = service.processor.tasks.find(f => f.type == TaskType.Pomodoro);
+      this.breakTask = service.processor.tasks.find(f => f.type == TaskType.Break);
+      this.longBreakTask = service.processor.tasks.find(f => f.type == TaskType.LongBreak);
+    });
+
+  }
 
   completeTask() {
-    this.service.publish({
-      type: EventType.TaskCompleted,
-      date: new Date(),
-      args: 0
-    });
+
+    let index = 0;
+
+    if (this.service.timer.pomodoroState == TaskType.Pomodoro)
+      index = this.service.processor.tasks.indexOf(this.pomodoroTask);
+
+    if (this.service.timer.pomodoroState == TaskType.Break)
+      index = this.service.processor.tasks.indexOf(this.breakTask);
+
+    if (this.service.timer.pomodoroState == TaskType.LongBreak)
+      index = this.service.processor.tasks.indexOf(this.longBreakTask);
+
+    if (index >= 0)
+      this.service.publish({
+        type: EventType.TaskCompleted,
+        date: new Date(),
+        args: index
+      });
   }
 }
